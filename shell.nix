a31c8eed010572f42e4f41298c79eb5568bc75c2
Ernad Husremovic <hernad@bring.out.ba>
2025-06-28 08:48:20 +0200

    shell.nix OK

--- shell.nix
+++ shell.nix
@@ -1,18 +1,36 @@
-{ pkgs ? import <nixpkgs-25.05> {} }:
+{ pkgs ? import <nixpkgs-25.05> { 
+    config = { 
+      android_sdk.accept_license = true; 
+      allowUnfree = true; 
+    }; 
+  } 
+}:
 
 with pkgs;
 
+let
+  # Use composeAndroidPackages with the correct arguments for nixpkgs-25.05
+  androidComposition = androidenv.composeAndroidPackages {
+    platformVersions = [ "31" ];
+    buildToolsVersions = [ "31.0.0" ];
+    cmdLineToolsVersion = "latest";
+  };
+
+  # The actual SDK is available under the 'androidsdk' attribute of the composition.
+  androidSdk = androidComposition.androidsdk;
+
+in
 mkShell {
   buildInputs = [
-    jdk
-    (androidenv {
-      buildTools = [ "30.0.3" ];
-      platforms  = [ "android-30" ];
-      extraTools = [
-        gradle
-        androidenv.androidPkgs.tools
-        androidenv.androidPkgs.platform-tools
-      ];
-    })
+    jdk11
+    gradle
+    androidSdk
   ];
+
+  # Set the necessary environment variables for Android tools and Gradle.
+  shellHook = ''
+    export ANDROID_SDK_ROOT="${androidSdk}/libexec/android-sdk"
+    export ANDROID_HOME="$ANDROID_SDK_ROOT"
+    export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_SDK_ROOT/build-tools/31.0.0/aapt2"
+  '';
 }
